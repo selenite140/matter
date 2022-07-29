78cc583797441e29e4a9778d1660a01b9179aee5
Marius Tache <marius.tache@nxp.com>
2022-07-29 09:03:27 -0700

    [K32W0] Change KVS message
    * Matter keys that are not found in persistent storage
    no longer output an "error" message.
    * Updated sdk patch to use the correct env variable.
    
    Signed-off-by: Marius Tache <marius.tache@nxp.com>

--- third_party/nxp/k32w0_sdk/sdk_fixes/patch_k32w_sdk.sh
+++ third_party/nxp/k32w0_sdk/sdk_fixes/patch_k32w_sdk.sh
@@ -15,8 +15,8 @@ convert_to_dos() {
 SOURCE=${BASH_SOURCE[0]}
 SOURCE_DIR=$(cd "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)
 
-convert_to_dos "$NXP_K32W061_SDK_ROOT"/middleware/wireless/framework/OSAbstraction/Source/fsl_os_abstraction_free_rtos.c
-patch -N --binary -d "$NXP_K32W061_SDK_ROOT"/middleware/wireless/framework/OSAbstraction/Source/ -p1 <"$SOURCE_DIR/fsl_os_abstraction_free_rtos_c.patch"
+convert_to_dos "$NXP_K32W0_SDK_ROOT"/middleware/wireless/framework/OSAbstraction/Source/fsl_os_abstraction_free_rtos.c
+patch -N --binary -d "$NXP_K32W0_SDK_ROOT"/middleware/wireless/framework/OSAbstraction/Source/ -p1 <"$SOURCE_DIR/fsl_os_abstraction_free_rtos_c.patch"
 
 echo "K32W SDK 2.6.6 was patched!"
 exit 0
