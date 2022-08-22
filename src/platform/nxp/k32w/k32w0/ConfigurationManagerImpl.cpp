2ca47bcc21fb9ab598eed4d7f8f6af252e96de41
Eric Ocasio <eric.ocasio@nxp.com>
2022-08-22 10:52:43 -0500

    Remove GetSoftwareVersion and StoreSoftwareVersion from K33W0 ConfigurationManagerImpl.

--- src/platform/nxp/k32w/k32w0/ConfigurationManagerImpl.cpp
+++ src/platform/nxp/k32w/k32w0/ConfigurationManagerImpl.cpp
@@ -52,13 +52,6 @@ CHIP_ERROR ConfigurationManagerImpl::Init()
     CHIP_ERROR err;
     uint32_t rebootCount = 0;
 
-    // Save out software version on first boot
-    if (!K32WConfig::ConfigValueExists(K32WConfig::kConfigKey_SoftwareVersion))
-    {
-        err = StoreSoftwareVersion(CHIP_DEVICE_CONFIG_DEVICE_SOFTWARE_VERSION);
-        SuccessOrExit(err);
-    }
-
     if (K32WConfig::ConfigValueExists(K32WConfig::kCounterKey_RebootCount))
     {
         err = GetRebootCount(rebootCount);
@@ -116,21 +109,6 @@ CHIP_ERROR ConfigurationManagerImpl::GetTotalOperationalHours(uint32_t & totalOp
 CHIP_ERROR ConfigurationManagerImpl::StoreTotalOperationalHours(uint32_t totalOperationalHours)
 {
     return WriteConfigValue(K32WConfig::kCounterKey_TotalOperationalHours, totalOperationalHours);
-}
-
-CHIP_ERROR ConfigurationManagerImpl::GetSoftwareVersion(uint32_t & softwareVer)
-{
-    return ReadConfigValue(K32WConfig::kConfigKey_SoftwareVersion, softwareVer);
-}
-
-CHIP_ERROR ConfigurationManagerImpl::StoreSoftwareVersion(uint32_t softwareVer)
-{
-    // Bypass the ConfigurationManager wrapper for writing and use
-    // synchronized write from K32WConfig to ensure that the software
-    // version is correctly updated, especially when a new update is
-    // applied, which resets the device before the PDM write on idle
-    // actually happens.
-    return K32WConfig::WriteConfigValueSync(K32WConfig::kConfigKey_SoftwareVersion, softwareVer);
 }
 
 CHIP_ERROR ConfigurationManagerImpl::GetBootReason(uint32_t & bootReason)
