8f1f3c6f73406626e1e3c41e66b850f93a8abfcc
Marius Tache <marius.tache@nxp.com>
2022-09-05 08:11:18 -0700

    [MATTER-446][K32W0] Add delay when rebooting after OTA finishes
    A delay is needed in order to ensure that all pending actions
    are actually executed before rebooting with the new image. For
    example, if someone is subscribed to an event, some events may
    be missed by the subscriber: state transition from Downloading
    to Applying when doing OTA. The delay can be overwritten by
    setting the define to a new value.
    
    Signed-off-by: Marius Tache <marius.tache@nxp.com>

--- src/platform/nxp/k32w/k32w0/CHIPDevicePlatformConfig.h
+++ src/platform/nxp/k32w/k32w0/CHIPDevicePlatformConfig.h
@@ -117,6 +117,15 @@
     }
 #endif
 
+/**
+ * @def CHIP_DEVICE_LAYER_OTA_REBOOT_DELAY
+ *
+ * The delay before rebooting after an OTA process was finished.
+ */
+#ifndef CHIP_DEVICE_LAYER_OTA_REBOOT_DELAY
+#define CHIP_DEVICE_LAYER_OTA_REBOOT_DELAY 3000
+#endif // CHIP_DEVICE_LAYER_OTA_REBOOT_DELAY
+
 // ========== Platform-specific Configuration Overrides =========
 #ifndef CHIP_DEVICE_CONFIG_CHIP_TASK_STACK_SIZE
 #define CHIP_DEVICE_CONFIG_CHIP_TASK_STACK_SIZE (6 * 1024)
